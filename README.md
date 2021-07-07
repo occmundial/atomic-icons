eef27c4a89340dc55e9bb816ada31b13eebe6cee
Luis Fernando Díaz <ldiaz@occ.com.mx>
2021-07-07 18:27:06 -0500

    Cambios en index y readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Candy:candy:Wrapper
 
-Candy:candy:Wrapper is a boilerplate system to create new Next.js applications with some implemented tools like OCC Atomic and Apollo Client, configured to point to the CandysQL endpoint. It's working with Webpack 5 and Module Federation.
+Candy:candy:Wrapper is a boilerplate system to create new Next.js applications with some implemented tools like OCC Atomic and Apollo Client, configured to point to the CandysQL endpoint and working with server-side rendering. It's working with Webpack 5 and Module Federation.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head'
 import { useQuery } from '@apollo/client'
-import { Text, Flexbox, Grid } from '@occmundial/occ-atomic'
+import { Text, Flexbox, Grid, Card } from '@occmundial/occ-atomic'
 import { EXAMPLE_QUERY } from 'graphql/queries/example'
 import { initializeApollo } from '@/graphql/apollo'
 
@@ -24,11 +24,13 @@ export default function Home() {
             height: '100vh'
           }}
         >
-          <Text hero>Candy🍬Wrapper</Text>
-          <Text center large topBase bottomSmall>Candy Wrapper is a boilerplate system to create new Next.js applications with some implemented tools like OCC Atomic and Apollo Client, configured to point to the CandysQL endpoint. It's working with Webpack 5 and Module Federation.</Text>
-          {data && (
-            <Text subheading strong info>{JSON.stringify(data)}</Text>
-          )}
+          <Card raisable>
+            <Text center hero secondary>Candy🍬Wrapper</Text>
+            <Text center large topBase bottomSmall><strong>Candy🍬Wrapper</strong> is a boilerplate system to create new Next.js applications with some implemented tools like OCC Atomic and Apollo Client, configured to point to the CandysQL endpoint and working with server-side rendering. It's working with Webpack 5 and Module Federation.</Text>
+            {data && (
+              <Text center subheading strong info>{JSON.stringify(data)}</Text>
+            )}
+          </Card>
         </Flexbox>
       </Grid>
     </div>
