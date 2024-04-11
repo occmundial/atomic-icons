2067e0931aa0679b7824f532a210ff456b68bb2b
Luis Fernando Díaz <ldiaz@occ.com.mx>
2024-04-11 12:39:20 -0600

    ci: Read credentials manually from env vars

--- scripts/uploadSprite.ts
+++ scripts/uploadSprite.ts
@@ -4,7 +4,13 @@ import fs from 'fs'
 import path from 'path'
 
 const version = process.argv[2]
-AWS.config.update({ region: 'us-west-2' })
+AWS.config.update({
+  region: 'us-west-2',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+})
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
 const uploadParams: PutObjectRequest = {
